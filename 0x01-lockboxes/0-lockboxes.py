851d0cb6cb57a211d81b78981be876a89c313932
KeaMohlala <keamogetsemhll@gmail.com>
2024-08-01 22:54:50 +0000

    change metrhod to check unlocked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,24 +8,18 @@ def canUnlockAll(boxes):
     Args: boxes - a list of lists
     Return: true if all boxes can be opened else return false
     """
-    # Initialize a set to keep track of visited boxes
-    visited_boxes = set()
-
-    # Start from the first box (index 0)
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
     current_box_keys = boxes[0]
 
     while current_box_keys:
-        # Mark the current box as visited
-        visited_boxes.add(len(current_box_keys) - 1)
+        key = current_box_keys.pop()
+        if key < n and not unlocked[key]:
+            # mark as unlocked
+            unlocked[key] = True
+            # add keys from this box
+            current_box_keys.extend(boxes[key])
 
-        # Update the current box keys with the keys found in the current box
-        next_box_keys = []
-        for key in current_box_keys:
-            if key < len(boxes):
-                next_box_keys.extend(boxes[key])
-
-        # Move to the next box
-        current_box_keys = next_box_keys
-
-    # Check if all boxes were visited
-    return len(visited_boxes) == len(boxes)
+    # Check if all boxes are unlocked
+    return all(unlocked)
