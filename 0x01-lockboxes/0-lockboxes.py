bdd1812a9b92e11d6f9815d534d1d2ba12fdfde7
KeaMohlala <keamogetsemhll@gmail.com>
2024-08-01 21:40:25 +0000

    create canunlockall function

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" module to document the lockboxes function"""
+
+def canUnlockAll(boxes):
+    """
+    method to determine if all boxes can be opened
+    Args: boxes - a list of lists
+    Return: true if all boxes can be opened else return false
+    """
+    #Initialize a set to keep track of visited boxes
+    visited_boxes = set()
+
+    #Start from the first box (index 0)
+    current_box_keys = boxes[0]
+
+    while current_box_keys:
+        # Mark the current box as visited
+        visited_boxes.add(len(current_box_keys) - 1)
+
+        # Update the current box keys with the keys found in the current box
+        next_box_keys = []
+        for key in current_box_keys:
+            if key < len(boxes):
+                next_box_keys.extend(boxes[key])
+
+        # Move to the next box
+        current_box_keys = next_box_keys
+
+    # Check if all boxes were visited
+    return len(visited_boxes) == len(boxes)
